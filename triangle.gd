86d74227055c3ea4575df4e588afb50004a127c9
RoseKikiQ <Rosellucyc@gmail.com>
2025-09-27 16:58:00 -0600

    为首之人行得通的长方体预防干预别国

--- triangle.gd
+++ triangle.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 var jumping = false
-const MARGIN = 50
-
+const b = 50
+var Key_collected = false
+var Has_Key = false
+var Not_has_key = true
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	velocity = input_dir * 500
@@ -13,14 +15,14 @@ func _physics_process(delta: float) -> void:
 		jumping = false
 	if jumping:
 		$Icon.scale+=Vector2(delta,delta)
-		$Icon.scale = $Icon.scale.clamp(Vector2(1,1), Vector2(1.5, 1.5))
+		$Icon.scale = $Icon.scale.clamp(Vector2(0.75,0.75), Vector2(1.5, 1.5))
 	if not jumping:
 		$Icon.scale-=Vector2(delta,delta)
-		$Icon.scale = $Icon.scale.clamp(Vector2(1,1), Vector2(1.5, 1.5))
-	if position.x < MARGIN:
-		position.x = MARGIN
-	if position.y < MARGIN:
-		position.y = MARGIN
+		$Icon.scale = $Icon.scale.clamp(Vector2(0.75,0.75), Vector2(1.5, 1.5))
+	if position.x < b:
+		position.x = b
+	if position.y < b:
+		position.y = b
 	if position.y > 600:
 		position.y = 600
 	if position.x > 1100:
